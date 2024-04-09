bc302212b4ba636f10c33cf6afc4902cf19bc8d7
tristanff <tristan.flechard@hotmail.fr>
2024-04-09 19:23:06 -0300

    First implementation of TP2 Game

---
+++ main.py
@@ -0,0 +1,61 @@
+from src.character import Warrior,Archer,Defender,Infiltrator
+
+
+my_warrior = Warrior(height=1.75, strength_items=52.5, agility_items=22.5, proficiency_items=31.0, resistance_items=21.0, life_items=23.0)
+my_archer = Archer(height=1.62, strength_items=21.0, agility_items=52.0, proficiency_items=31.0, resistance_items=20.5, life_items=25.5)
+my_defender = Defender(height=1.93, strength_items=51.0, agility_items=20.0, proficiency_items=21.0, resistance_items=50.0, life_items=8.0)
+my_infiltrator = Infiltrator(height=1.65, strength_items=31.5, agility_items=41.0, proficiency_items=41.5, resistance_items=21.0, life_items=15.0)
+
+
+print("Warrior Performance : ", my_warrior.performance())
+print("Archer Performance : ", my_archer.performance())
+print("Defender Perfomance : ", my_defender.performance())
+print("Infiltrator Perfomance : ", my_infiltrator.performance())
+
+
+
+#Basic function to create random personnages with items = 150 - 5 by 5 for each items
+def fabrique_personnages():
+    personnages = []
+
+    for strength_items in range(0, 151, 5):
+        for agility_items in range(0, 151 - strength_items, 5):
+            for proficiency_items in range(0, 151 - strength_items - agility_items, 5):
+                for resistance_items in range(0, 151 - strength_items - agility_items - proficiency_items, 5):
+                    life_items = 150 - (strength_items + agility_items + proficiency_items + resistance_items)
+                    if life_items % 5 == 0:
+                        height = 1.7  #Default value for testing
+                        personnages.append(Warrior(height, strength_items, agility_items, proficiency_items, resistance_items, life_items))
+                        personnages.append(Archer(height, strength_items, agility_items, proficiency_items, resistance_items, life_items))
+                        personnages.append(Infiltrator(height, strength_items, agility_items, proficiency_items, resistance_items, life_items))
+                        personnages.append(Defender(height, strength_items, agility_items, proficiency_items, resistance_items, life_items))
+
+    return personnages
+
+personnages = fabrique_personnages()
+
+
+best_personnage = max(personnages, key=lambda x: x.performance())
+for idx, personnage in enumerate(personnages):
+    print(f"Personnage {idx+1} ({type(personnage).__name__}):")
+    print(f"  - Strength Items: {personnage.strength_items}")
+    print(f"  - Agility Items: {personnage.agility_items}")
+    print(f"  - Proficiency Items: {personnage.proficiency_items}")
+    print(f"  - Resistance Items: {personnage.resistance_items}")
+    print(f"  - Life Items: {personnage.life_items}")
+    print(f"  - Performance : {personnage.performance()}")
+    print()
+
+
+best_personnage = max(personnages, key=lambda x: x.performance())
+
+print(f"Best_Personnage {idx + 1} ({type(best_personnage).__name__}):")
+print(f"  - Strength Items: {best_personnage.strength_items}")
+print(f"  - Agility Items: {best_personnage.agility_items}")
+print(f"  - Proficiency Items: {best_personnage.proficiency_items}")
+print(f"  - Resistance Items: {best_personnage.resistance_items}")
+print(f"  - Life Items: {best_personnage.life_items}")
+print(f"  - Performance : {best_personnage.performance()}")
+
+
+
---
+++ src/character.py
@@ -0,0 +1,85 @@
+import math
+
+class Personnage:
+    def __init__(self, height, strength_items, agility_items, proficiency_items, resistance_items, life_items):
+        self.height = height
+        self.strength_items = strength_items
+        self.agility_items = agility_items
+        self.proficiency_items = proficiency_items
+        self.resistance_items = resistance_items
+        self.life_items = life_items
+
+        #Raise an error if sum of items not equal to 150
+        total_items = strength_items + agility_items + proficiency_items + resistance_items + life_items
+        if total_items != 150:
+            raise ValueError("La somme des attributs _items doit être égale à 150.")
+
+
+
+
+    def performance(self):
+        pass
+
+class Warrior(Personnage):
+    def performance(self):
+        strength_coeff = 100 * math.tanh(0.01 * self.strength_items)
+        agility_coeff = math.tanh(0.01 * self.agility_items)
+        proficiency_coeff = 0.6 * math.tanh(0.01 * self.proficiency_items)
+        resistance_coeff = math.tanh(0.01 * self.resistance_items)
+        life_coeff = 100 * math.tanh(0.01 * self.life_items)
+
+        attack_modifier = 0.5 - (3*self.height - 5)**4 + (3*self.height - 5)**2 + self.height/2
+        defense_modifier = 2 + (3*self.height - 5)**4 - (3*self.height - 5)**2 - self.height/2
+
+        attack = (agility_coeff + proficiency_coeff) * strength_coeff * attack_modifier
+        defense = (resistance_coeff + proficiency_coeff) * life_coeff * defense_modifier
+
+        return 0.6 * attack + 0.4 * defense
+
+class Archer(Personnage):
+    def performance(self):
+        strength_coeff = 100 * math.tanh(0.01 * self.strength_items)
+        agility_coeff = math.tanh(0.01 * self.agility_items)
+        proficiency_coeff = 0.6 * math.tanh(0.01 * self.proficiency_items)
+        resistance_coeff = math.tanh(0.01 * self.resistance_items)
+        life_coeff = 100 * math.tanh(0.01 * self.life_items)
+
+        attack_modifier = 0.5 - (3*self.height - 5)**4 + (3*self.height - 5)**2 + self.height/2
+        defense_modifier = 2 + (3*self.height - 5)**4 - (3*self.height - 5)**2 - self.height/2
+
+        attack = (agility_coeff + proficiency_coeff) * strength_coeff * attack_modifier
+        defense = (resistance_coeff + proficiency_coeff) * life_coeff * defense_modifier
+
+        return 0.9 * attack + 0.1 * defense
+
+class Defender(Personnage):
+    def performance(self):
+        strength_coeff = 100 * math.tanh(0.01 * self.strength_items)
+        agility_coeff = math.tanh(0.01 * self.agility_items)
+        proficiency_coeff = 0.6 * math.tanh(0.01 * self.proficiency_items)
+        resistance_coeff = math.tanh(0.01 * self.resistance_items)
+        life_coeff = 100 * math.tanh(0.01 * self.life_items)
+
+        attack_modifier = 0.5 - (3*self.height - 5)**4 + (3*self.height - 5)**2 + self.height/2
+        defense_modifier = 2 + (3*self.height - 5)**4 - (3*self.height - 5)**2 - self.height/2
+
+        attack = (agility_coeff + proficiency_coeff) * strength_coeff * attack_modifier
+        defense = (resistance_coeff + proficiency_coeff) * life_coeff * defense_modifier
+
+        return 0.1 * attack + 0.9 * defense
+
+class Infiltrator(Personnage):
+    def performance(self):
+        strength_coeff = 100 * math.tanh(0.01 * self.strength_items)
+        agility_coeff = math.tanh(0.01 * self.agility_items)
+        proficiency_coeff = 0.6 * math.tanh(0.01 * self.proficiency_items)
+        resistance_coeff = math.tanh(0.01 * self.resistance_items)
+        life_coeff = 100 * math.tanh(0.01 * self.life_items)
+
+        attack_modifier = 0.5 - (3*self.height - 5)**4 + (3*self.height - 5)**2 + self.height/2
+        defense_modifier = 2 + (3*self.height - 5)**4 - (3*self.height - 5)**2 - self.height/2
+
+        attack = (agility_coeff + proficiency_coeff) * strength_coeff * attack_modifier
+        defense = (resistance_coeff + proficiency_coeff) * life_coeff * defense_modifier
+
+        return 0.8 * attack + 0.3 * defense
